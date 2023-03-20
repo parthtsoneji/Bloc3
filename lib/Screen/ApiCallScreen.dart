8808b4f9318468ef7065b92d9dcfab28e508f719
Parth Soneji <parthsoneji.softrefine@gmail.com>
2023-03-20 10:13:52 +0530

    Fourth Commit

--- lib/Screen/ApiCallScreen.dart
+++ lib/Screen/ApiCallScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:herokuapp/Bloc/bloc_heroku_bloc.dart';
-import 'package:snackbar/snackbar.dart';
 import '../Services/HerokuModel.dart';
 
 class ApiCallScreen extends StatefulWidget {
@@ -13,70 +12,64 @@ class ApiCallScreen extends StatefulWidget {
 
 class _ApiCallScreenState extends State<ApiCallScreen> {
   int index = 0;
-  bool _canShowButton = false;
-  bool builer = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       children: [
-        if (!_canShowButton)
+
+
+        BlocListener<HerokuBloc, HerokuState>(
+          listener: (context, state) {
+            if (state is HerokuLoadingState) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Center(child: CircularProgressIndicator())));
+            }
+            if(state is HerokuLoadedState){
+              HerokuPizza data =  state.heroku;
+              ScaffoldMessenger.of(context).showSnackBar( SnackBar(content:  pizzaWidget(data)));
+            }
+          },
+          child:
           Padding(
-            padding: const EdgeInsets.only(top: 100),
+            padding: const EdgeInsets.only(top: 100,left: 100),
             child: ElevatedButton(
               onPressed: () {
                 context.read<HerokuBloc>().add(ClickEvent(HerokuPizza()));
-                setState(() {
-                  _canShowButton = true;
-                });
+
               },
               child: const Text('Click Me..!!!'),
             ),
-          ),
-        BlocListener<HerokuBloc, HerokuState>(
-          listener: (context, state) {
-            // if (state is HerokuLoadingState) {
-            //   ScaffoldMessenger.of(context)
-            //       .showSnackBar(const SnackBar(content: Text("Loading")));
-            // }
-            if (state is HerokuLoadingState) {
-              _canShowButton = false;
-              ScaffoldMessenger.of(context)..showSnackBar(const SnackBar(
-                    content: Center(child: CircularProgressIndicator())));
-            }
-            else if(state is HerokuLoadedState){
-              HerokuPizza data =  state.heroku;
-              Scaffold.of(context).setState(() {
-                pizzaWidget(data);
-              });
-            }
-          },
-          child: Text("Hello")
+          )
         )
       ],
     ));
   }
 
   Widget pizzaWidget(HerokuPizza data) {
-    return Expanded(
-      flex: 1,
-      child: ListView.builder(
-          itemCount: data.recipes!.length,
-          itemBuilder: (_, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-              child: Card(
-                  child: ListTile(
-                leading: Image.network(data.recipes![index].imageUrl.toString(),
-                    width: 80),
-                title: Text(
-                  data.recipes![index].title.toString(),
-                  style: const TextStyle(color: Colors.black),
-                ),
-              )),
-            );
-          }),
+    return Column(
+      children: [
+        Expanded(
+          flex: 1,
+          child: ListView.builder(
+              itemCount: data.recipes!.length,
+              itemBuilder: (_, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  child: Card(
+                      child: ListTile(
+                    leading: Image.network(data.recipes![index].imageUrl.toString(),
+                        width: 80),
+                    title: Text(
+                      data.recipes![index].title.toString(),
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  )),
+                );
+              }),
+        ),
+      ],
     );
   }
 }
